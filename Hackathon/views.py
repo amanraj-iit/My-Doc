ffe879271c635b964b637105d66bdc53e553ef42
amanraj-iit <77458628+amanraj-iit@users.noreply.github.com>
2021-09-25 01:54:51 +0530

    Delete Hackathon directory

--- Hackathon/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request, 'base.html')
-
-def Contact(request):
-    return render(request, "contact.html")
